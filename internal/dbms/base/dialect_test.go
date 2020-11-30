0ef41931671f698b379bb7f50f57070da4ca2e95
Ben Vezzani <dotvezz@gmail.com>
2020-11-29 21:17:20 -0500

    Add tests for base dialect implementation (#5)

--- internal/dbms/base/dialect_test.go
+++ internal/dbms/base/dialect_test.go
@@ -2,6 +2,7 @@ package base
 
 import (
 	"github.com/dotvezz/yoyo/internal/datatype"
+	"github.com/dotvezz/yoyo/internal/schema"
 	"testing"
 )
 
@@ -54,4 +55,120 @@ func TestBase_TypeString(t *testing.T) {
 			}
 		})
 	}
+}
+
+// We expect the "base" dialect to panic on every method except TypeString
+func TestBase_Panics(t *testing.T) {
+	didPanic := func(f func()) (res bool) {
+		defer func() {
+			if r := recover(); r != nil {
+				res = true
+			}
+		}()
+
+		f()
+
+		return
+	}
+
+	b := Base{}
+
+	t.Run("AddReference", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.AddReference("", "", schema.Database{}, schema.Reference{})
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("CreateTable", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_ = b.CreateTable("", schema.Table{})
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("AddIndex", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_ = b.AddIndex("", "", schema.Index{})
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("AddColumn", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_ = b.AddColumn("", "", schema.Column{})
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("ListTables", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.ListTables()
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("ListColumns", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.ListColumns("")
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("ListIndices", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.ListIndices("")
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("ListReferences", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.ListReferences("")
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("GetColumn", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.GetColumn("", "")
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("GetIndex", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.GetIndex("", "")
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
+
+	t.Run("GetReference", func(t *testing.T) {
+		panicked := didPanic(func() {
+			_, _ = b.GetReference("", "")
+		})
+		if !panicked {
+			t.Errorf("Expected a panic but didn't see one")
+		}
+	})
 }
